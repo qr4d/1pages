4031e9601985ca1552263bd001a744fad3d94e24
qinrui <qinrui@qr4d.com>
2025-02-22 17:22:55 +0800

    更新网站配置：修改网站标题、描述和社交媒体链接

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,9 @@
+// ... existing code ...
+<header className="site-header">
+  <h1>{websiteConfig.title}</h1>
+  <p className="site-description">{websiteConfig.description}</p>
+  <div className="author-info">
+    <span>{websiteConfig.author}</span>
+  </div>
+</header>
+// ... existing code ...
---
+++ src/components/SEO/SEO.jsx
@@ -0,0 +1,8 @@
+// ... existing code ...
+<Helmet>
+  <title>{websiteConfig.title}</title>
+  <meta name="description" content={websiteConfig.description} />
+  <meta name="author" content={websiteConfig.author} />
+  <link rel="canonical" href={websiteConfig.url} />
+</Helmet>
+// ... existing code ...
